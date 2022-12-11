7be732d274897860f22c14995f7b321eab7fedda
hannahscript <h4nn4h777@protonmail.com>
2022-12-12 00:19:14 +0100

    Improve day 11

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -22,7 +22,7 @@ struct Operation {
 #[derive(Debug, Clone)]
 struct Monkey {
     id: usize,
-    items: Vec<MonkeyNumber>,
+    items: Vec<u64>,
     operation: Operation,
     test: u64,
     when_true: usize,
@@ -48,44 +48,6 @@ impl Monkey {
     }
 }
 
-#[derive(Debug, Clone)]
-struct MonkeyNumber {
-    // (mod, val)
-    classes: Vec<(u64, u64)>
-}
-
-impl MonkeyNumber {
-    fn new(num: u64, divisors: &Vec<u64>) -> MonkeyNumber {
-        MonkeyNumber {
-            classes: divisors.iter().map(|&d| (d, num % d)).collect()
-        }
-    }
-    
-    fn add(self: &MonkeyNumber, other: &MonkeyNumber, divide_by_three: bool) -> MonkeyNumber {
-        let mut classes = vec![];
-        for i in 0..self.classes.len() {
-            let md = self.classes[i].0;
-            classes.push((md, ((self.classes[i].1 + other.classes[i].1) / if divide_by_three { 3 } else { 1 }) % md))
-        }
-        
-        MonkeyNumber {
-            classes
-        }
-    }
-
-    fn mult(self: &MonkeyNumber, other: &MonkeyNumber, divide_by_three: bool) -> MonkeyNumber {
-        let mut classes = vec![];
-        for i in 0..self.classes.len() {
-            let md = self.classes[i].0;
-            classes.push((md, ((self.classes[i].1 * other.classes[i].1) / if divide_by_three { 3 } else { 1 }) % md))
-        }
-
-        MonkeyNumber {
-            classes
-        }
-    }
-}
-
 fn read_input() -> Vec<Monkey> {
     let mut m = Monkey::new();
 
@@ -96,7 +58,6 @@ fn read_input() -> Vec<Monkey> {
     let lines = contents.split("\r\n");
     
     let mut result: Vec<Monkey> = vec![];
-    let mut all_items: Vec<Vec<u64>> = vec![];
     for line in lines {
         if line.starts_with("Monkey") {
             m = Monkey::new();
@@ -104,8 +65,7 @@ fn read_input() -> Vec<Monkey> {
             m.id = id[..1].parse().unwrap();
         } else if line.contains("Starting items") {
             let (_, items_raw) = line.split_at(18);
-            let items: Vec<u64> = items_raw.split(", ").into_iter().map(|i| i.parse().unwrap()).collect();
-            all_items.push(items);
+            m.items = items_raw.split(", ").into_iter().map(|i| i.parse().unwrap()).collect();
         } else if line.contains("Operation") {
             let (_, op_raw) = line.split_at(19);
             m.operation = parse_op(op_raw);
@@ -120,13 +80,6 @@ fn read_input() -> Vec<Monkey> {
             m.when_false = id.parse().unwrap();
             result.push(m.clone());
         }
-    }
-    println!("All items {:?}", all_items);
-    
-    // Generate monkey numbers
-    let divisors: Vec<u64> = result.iter().map(|m| m.test).collect();
-    for i in 0..result.len() {
-        result[i].items = all_items[i].iter().map(|&item| MonkeyNumber::new(item, &divisors)).collect();
     }
     
     result
@@ -148,42 +101,55 @@ fn parse_op(text: &str) -> Operation {
     }
 }
 
-fn apply_op(worry: &MonkeyNumber, op: &Operation, divisors: &Vec<u64>, divide_by_three: bool) -> MonkeyNumber {
+fn apply_m(worry: &u64, op: &Operation, m: u64) -> u64 {
     let left = match &op.left {
-        Operand::OLD => worry.clone(),
-        Operand::LITERAL(n) => MonkeyNumber::new(*n, &divisors)
+        Operand::OLD => worry,
+        Operand::LITERAL(n) => n
     };
     let right = match &op.right {
-        Operand::OLD => worry.clone(),
-        Operand::LITERAL(n) => MonkeyNumber::new(*n, &divisors)
+        Operand::OLD => worry,
+        Operand::LITERAL(n) => n
     };
     match op.operator {
-        Operator::PLUS => left.add(&right, divide_by_three),
-        Operator::TIMES => left.mult(&right, divide_by_three)
+        Operator::PLUS => (left + right) % m,
+        Operator::TIMES => (left * right) % m
     }
 }
 
-fn monkey_around(id: usize, monkeys: &mut Vec<Monkey>, divide_by_three: bool) {
-    let mut transfers: Vec<(usize, MonkeyNumber)> = vec![];
+fn apply_s(worry: &u64, op: &Operation) -> u64 {
+    let left = match &op.left {
+        Operand::OLD => worry,
+        Operand::LITERAL(n) => n
+    };
+    let right = match &op.right {
+        Operand::OLD => worry,
+        Operand::LITERAL(n) => n
+    };
+    match op.operator {
+        Operator::PLUS => (left + right) / 3,
+        Operator::TIMES => (left * right) / 3
+    }
+}
+
+fn monkey_around<F>(id: usize, monkeys: &mut Vec<Monkey>, apply: F)
+    where F: Fn(&u64, &Operation) -> u64 {
+    let mut transfers: Vec<(usize, u64)> = vec![];
     {
-        let divisors: Vec<u64> = monkeys.iter().map(|m| m.test).collect();
         let monkey = &mut monkeys[id];
         
         for item in &monkey.items {
             monkey.total_inspections += 1;
-            let mut worry = apply_op(&item, &monkey.operation, &divisors, divide_by_three);
-            //println!("Worry op {:?} {:?} ==> {:?}", &monkey.operation, item, worry);
-            if worry.classes[id].1 == 0 {
-                transfers.push((monkey.when_true, worry.clone()));
+            let worry = apply(item, &monkey.operation);
+            
+            if worry % monkey.test == 0 {
+                transfers.push((monkey.when_true, worry));
             } else {
-                transfers.push((monkey.when_false, worry.clone()));
+                transfers.push((monkey.when_false, worry));
             }
         }
 
         monkey.items.clear();
     }
-
-    //println!("TRANSFERS {:?}", transfers);
     
     for (other_id, worry) in transfers {
         monkeys[other_id].items.push(worry);
@@ -196,36 +162,29 @@ pub fn solve() {
     let part2 = solve_part2(&mut input);
 
     println!("Day 11");
-    /*
-    Part 1 broke with the addition of modular arithmetic (cause of the division by 3 bit) and i cant be bothered to revert the code
-    i wrote for it earlier and threw away. but same principle with u64 instead of monkey numbers
-     */
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
 }
 
-fn simulate(rounds: u32, monkeys: &mut Vec<Monkey>, divide_by_three: bool) -> u64 {
-    for r in 0..rounds {
-        if r % 100 == 0 {
-            //println!("Round {}/{} done", r + 1, rounds);
-        }
+fn simulate<F>(rounds: u32, monkeys: &mut Vec<Monkey>, apply: F) -> u64
+    where F: Fn(&u64, &Operation) -> u64 {
+    for _ in 0..rounds {
         for id in 0..monkeys.len() {
-            monkey_around(id, monkeys, divide_by_three);
+            monkey_around(id, monkeys, &apply);
         }
-        //println!("{:?}\n\n", monkeys[0].items);
     }
 
     let mut inspections: Vec<u64> = monkeys.iter().map(|m| m.total_inspections).collect();
     inspections.sort();
-    inspections.reverse();
-
-    inspections[0] * inspections[1]
+    inspections[inspections.len() - 2 ..].iter().product()
+    
 }
 
 fn solve_part1(monkeys: &mut Vec<Monkey>) -> u64 {
-    simulate(20, monkeys, true)
+    simulate(20, monkeys, apply_s)
 }
 
 fn solve_part2(monkeys: &mut Vec<Monkey>) -> u64 {
-    simulate(10000, monkeys, false)
+    let m = monkeys.iter().map(|m| m.test).product();
+    simulate(10000, monkeys, |worry, op| apply_m(worry, op, m))
 }
